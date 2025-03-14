4467f67d6520f4c8a76c9e61f644b4ffb7aa675a
Roland Stedy <stedyroland@gmail.com>
2025-03-15 00:55:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className='text-gradient'>Movies</span> You'll Enjoy Without Hassle
+            Find <span className='text-gradient'>Movies</span> You'll Enjoy With Family and Friends
           </h1>
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
