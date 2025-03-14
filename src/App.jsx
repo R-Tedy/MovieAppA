d5b57183decbd803259df10ae4c64032611658c3
Roland Stedy <stedyroland@gmail.com>
2025-03-14 17:08:03 +0300

    appwrite counter

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
 import { useDebounce } from 'react-use'
+import { updateSearchCount } from './appwrite'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3/'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -46,6 +47,10 @@ const App = () => {
       }
 
       setMovieList(data.results || [])
+
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       console.log(`Error Fetching movies ${error}`)
       setErrorMessage('Error Fetching Movies. Please Try Again Later.')
